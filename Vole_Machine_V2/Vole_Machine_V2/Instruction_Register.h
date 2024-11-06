eac077332be0496e1ad50f692314feca6eda8c32
Mina Maher Tawfik <149970275+minamaher005@users.noreply.github.com>
2024-11-06 23:54:49 +0200

    Delete Vole_Machine_V2 directory

--- Vole_Machine_V2/Vole_Machine_V2/Instruction_Register.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-using namespace std;
-class InstructionRegister {
-private:
-    string _Current_Instruction;
-public:
-    void load(const string& inst) { _Current_Instruction = inst; }
-   string get_Instruction()  { return _Current_Instruction; }
-};
